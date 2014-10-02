38fd50293a54f6fb8cb4b003a94e4923712c87cf
ei12012 <ei12012@fe.up.pt>
2014-10-02 18:17:37 +0100

    Update protocol.c

--- protocol.c
+++ protocol.c
@@ -1,9 +1,16 @@
 #include "protocol.h"
 
-unsigned char set[5];
+Byte set[5];
+Byte ua[5];
 
 set[0] = F;
 set[1] = A;
 set[2] = C;
 set[3] = set[1]^set[2];
 set[4] = F;
+
+ua[0] = set[0];
+ua[1] = set[1];
+ua[2] = set[2];
+ua[3] = set[3];
+ua[4] = set[4];
