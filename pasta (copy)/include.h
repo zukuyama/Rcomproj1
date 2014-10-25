184e34a1b551ae76e579a5875a2055b18e49ce50
root <root@tux54.netlab.fe.up.pt>
2014-10-25 15:52:26 +0100

    saviiiing

---
+++ pasta (copy)/include.h
@@ -0,0 +1,24 @@
+//
+//  include.h
+//  RCOM_PROJ1
+//
+//  Created by GROUP_.
+//
+
+#ifndef RCOM_PROJ1_include_h
+#define RCOM_PROJ1_include_h
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include <math.h>
+
+#endif
